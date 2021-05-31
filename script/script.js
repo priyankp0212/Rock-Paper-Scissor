fa80d7a96ed6003dfeca86c7b31bf4d2a38f88a8
priyank0212 <76472430+priyankp0212@users.noreply.github.com>
2021-05-31 14:57:27 +0530

    logic completed

--- script/script.js
+++ script/script.js
@@ -1,1 +1,54 @@
 'use strict'
+
+const result  = document.getElementById('result');
+
+let [comuter_score, user_score] = [0, 0];
+
+let gameRules = {
+    Rock: {
+        Rock : 'draw',
+        Scissors: 'win',
+        Paper : 'lose'
+    },
+    Paper: {
+        Paper : 'draw',
+        Rock : 'win',
+        Scissors : 'lose'
+    },
+    Scissors : {
+        Scissors : 'draw',
+        Paper: 'win',
+        Rock : 'lose'
+    },
+};
+
+
+
+function clicked(input){
+    let choices = ['Rock', 'Paper', 'Scissors'];
+    let randomNumber = Math.trunc(Math.random() * 3);
+    let computer_choice = choices[randomNumber];
+    document.getElementById('user_choice').
+    innerHTML = `Computer Choose <span>${computer_choice.toUpperCase()}</span>`;
+    document.getElementById('computer_choice').
+    innerHTML = `You Choose <span>${input.toUpperCase()}</span>`;
+
+
+
+switch (gameRules[input][computer_choice]) {
+    case 'win':
+    result.innerText = 'You win';
+    user_score++;
+    break;
+    case 'lose':
+        result.innerText = 'You lose';
+        comuter_score++;
+        break;   
+        case 'draw':
+            result.innerText = 'You draw';
+            break;  
+}
+
+document.getElementById('computer_score').innerHTML = comuter_score ;
+document.getElementById('user_score').innerHTML = user_score ;
+}
